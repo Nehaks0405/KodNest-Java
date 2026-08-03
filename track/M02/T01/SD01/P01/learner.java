66bc1048dfe7550459eaced5cdc9bcb7f7246ffd
Nehaks0405 <nehasreeks0405@gmail.com>
2026-08-03 23:00:21 +0530

    D2 P2

---
+++ track/M02/T01/SD01/P01/learner.java
@@ -0,0 +1,13 @@
+package track.M02.T01.SD01.P01;
+
+public class learner {
+
+    public static void main(String[] args) {
+        String learnerName = "Salman";
+        int dailyPracticeHours = 8;
+        System.out.println("Name: " + learnerName);
+        System.out.println("Daily Practice: " + dailyPracticeHours + " Hours");
+    }
+}
+    
+
