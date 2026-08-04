610af016195942ce44877798301ed66a57670cef
Nehaks0405 <nehasreeks0405@gmail.com>
2026-08-04 11:31:58 +0530

    D3 P1

---
+++ track/M02/T01/SD01/P01/M02/T01/SD01/P02/main.java
@@ -0,0 +1,14 @@
+package track.M02.T01.SD01.P01.M02.T01.SD01.P02;
+
+public class main {
+    public static void main(String[] args){
+        System.out.println("Monday");
+        System.out.print("Tuesday");
+        System.out.println("Wednesday");
+        System.out.println("Thursday");
+        System.out.print("Friday");
+        System.out.println("Saturday");
+        System.out.print("Sunday");
+    }
+    
+}
