04b71c3b00be3b129bc704d149b471e6f521ba9c
Nehaks0405 <nehasreeks0405@gmail.com>
2026-08-04 20:05:20 +0530

    D3 P5

---
+++ track/M03/T01/SD01/P03/Placement.java
@@ -0,0 +1,13 @@
+package track.M03.T01.SD01.P03;
+
+public class Placement {
+    public static void main(String[] args){
+        int marks = 72;
+        int attendance = 80;
+        boolean activeBacklog = false;
+        boolean projectCompleted = true;
+        int mockInterviewScore = 65;
+        System.out.println("Improve Interview Skills");
+    }
+    
+}
