9591480f30ec13396d0cbfbfcade08e0e4d805d7
Nehaks0405 <nehasreeks0405@gmail.com>
2026-08-05 12:30:57 +0530

    D4 P3

---
+++ track/M02/T01/SD01/P01/M02/T01/SD01/P02/pgm3.java
@@ -0,0 +1,12 @@
+package track.M02.T01.SD01.P01.M02.T01.SD01.P02;
+
+public class pgm3 {
+    public static void main(String[] args){
+        int i=1;
+        while(i<=5){
+            System.out.println(i);
+            i++;
+        }
+    }
+    
+}
